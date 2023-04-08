3cc3236ea68c356bb8b669a6ed6114b3e99e06a2
Uday Reddy A <115851621+1udayreddy@users.noreply.github.com>
2023-04-08 17:32:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,6 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
 // deployment 
 __dirname = path.resolve();
 const staticPath = path.join(__dirname, '..', 'store', 'build');
